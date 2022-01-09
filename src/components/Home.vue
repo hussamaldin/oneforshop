ef63d01f2a2174e16365736a303c1e4cd16356e7
Your Name <you@example.com>
2022-01-09 08:47:52 +0200

    my therd change

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
         <div class="flex flex-row justify-between py-4 border-b  border-gray-200">
         <div class="justify-start">
             <div class="flex flex-row">
-                <div class="w-[30px] ml-2 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
+                <div class="w-[30px] ml-2 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
                 <div class="border-x ml-2 border-gray-500">
                     <button class="hover:bg-gray-200 ml-2 w-[60px] h-[50px]"><i class="fa fa-trash text-2xl"></i></button>
                     <button class="hover:bg-gray-200 ml-2 w-[60px] h-[50px]"><i class="fa fa-archive text-2xl"></i></button>
@@ -12,7 +12,7 @@
             </div>
         </div>
         <div class="justify-end">
-            <div class="flex flex-row">
+            <div class="flex flex-row items-center">
             <button class="hover:bg-gray-200 mr-2 w-[60px] h-[50px]"><i class="fa fa-bell text-2xl"></i></button>
             <button class="hover:bg-gray-200 mr-2 w-[60px] h-[50px]"><i class="fa fa-angle-left text-3xl"></i></button>
             <button class="hover:bg-gray-200 mr-2 w-[60px] h-[50px]"><i class="fa fa-angle-right text-3xl"></i></button>
@@ -22,67 +22,67 @@
         </div>
         <div class="w-full h-full flex flex-col p-1 overflow-x-auto">
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design  Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
         <div class="bg-white border-b border-gray-200 flex flex-row justify-between py-4 hover:bg-gray-100">
-        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[20px] h-[20px]"></div>
-        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-2xl"></i></button></div>
-        <div class="flex items-center px-2"><img :src="img" class="w-[50px] h-[50px] rounded-full mx-1"><p class="font-bold text-xl text-lg text-gray-900">OneForShop</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap"><p>This message From Me To Your erfect Website Design HHHHHH Welcome</p></div>
-        <div class="flex items-center mx-1 px-2 font-bold text-xl whitespace-nowrap">16 Aprail at 12:23 Am</div>
+        <div class="w-[30px] mx-1 h-[50px] flex items-center"><input type="checkbox" class="w-[16px] h-[16px]"></div>
+        <div class="flex items-center"><button><i class="fa fa-star-o hover:text-yellow-500 font-bold text-xl"></i></button></div>
+        <div class="flex items-center px-2"><img :src="img" class="w-[45px] h-[45px] rounded-full mx-1"><p class="font-bold text-lg text-gray-900">OneForShop</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap"><p>This message From Me To Your erfect Website Design Welcome</p></div>
+        <div class="flex items-center mx-1 px-2 font-bold text-lg whitespace-nowrap">16 Aprail at 12:23 Am</div>
         </div>
     </div>
 </template>
